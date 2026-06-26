cd7c9a63d50b74131d712612b6a15d07e2065945
Asmitha <asmithathavaradnam@gmail.com>
2026-06-26 12:28:38 +0530

    project folder added

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        medical: {
+          navy: '#0f3657',
+          blue: '#2563eb',
+          sky: '#eaf5ff',
+          emerald: '#059669',
+          mint: '#e8fff4'
+        }
+      }
+    }
+  },
+  plugins: []
+};
